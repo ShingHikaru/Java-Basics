c06ef48c84cd37169ebb5e98131fa5d15bdda4e4
Hikaru Shing <fiveh.5h@gmail.com>
2021-02-14 13:44:26 +0900

    UpdateCode

---
+++ 210214Construction01/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	BankAccount account = new BankAccount();
+	account.setAccountNumber("1993");
+	account.setBalance(19500);
+	account.setCustomerName("Shing");
+	account.setEmail("fiveh.5h@gmail.com");
+	account.setPhoneNum("080-7730-9347");
+
+        System.out.println();
+    }
+}
