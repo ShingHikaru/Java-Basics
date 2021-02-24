fa10f9488ee69b33c34d570043835b59d771f5ad
Hikaru Shing <fiveh.5h@gmail.com>
2021-02-24 16:13:15 +0900

    Updating Constructor 2

--- 210223Constructor/src/com/company/Main.java
+++ 210223Constructor/src/com/company/Main.java
@@ -1,8 +1,15 @@
 package com.company;
 
+import java.awt.*;
+
 public class Main {
 
     public static void main(String[] args) {
+        Dimensions dimensions = new Dimensions(20, 20,5);
+        Case theCase = new Case("220B","Dell","240",dimensions);
+
+        Monitor monitor = new Monitor("27inch Beast")
+
 
     }
 }
