0331b056873efd9d921fb7999800bbb2caaf7a5d
Hikaru Shing <fiveh.5h@gmail.com>
2021-02-26 22:08:02 +0900

    Updating Constructor

--- 210225RoomOfHouse/src/com/company/Main.java
+++ 210225RoomOfHouse/src/com/company/Main.java
@@ -12,6 +12,11 @@ public class Main {
         Ceiling ceiling = new Ceiling(12, 55);
 
         Bed bed = new Bed("Modern", 4, 3,2,1);
+
+        Lamp lamp = new Lamp("Classic", false, 75 );
+
+        Bedroom bedRoom = new Bedroom("Tims", wall1, wall2, wall3, wall4, ceiling, bed, lamp);
+
         
 
     }
