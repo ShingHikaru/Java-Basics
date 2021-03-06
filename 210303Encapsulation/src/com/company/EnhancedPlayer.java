54bb5a5c1d4c225d817e7991e4123e130a2f4b70
Hikaru Shing <fiveh.5h@gmail.com>
2021-03-06 23:23:18 +0900

    Updating Encap Project

--- 210303Encapsulation/src/com/company/EnhancedPlayer.java
+++ 210303Encapsulation/src/com/company/EnhancedPlayer.java
@@ -2,16 +2,36 @@ package com.company;
 
 public class EnhancedPlayer {
     public String name;
-    public int health = 100;
+    public int hitPoints = 100;
     public String weapon;
 
-    public EnhancedPlayer(String name, int health, String weapon) {
+    public EnhancedPlayer(String name, int hitPoints, String weapon) {
         this.name = name;
 
-        if(health >0 && health <= 100) {
-            
+        if(hitPoints >0 && hitPoints <= 100) {
+            this.hitPoints = hitPoints;
         }
-        this.health
 
+        this.weapon = weapon;
     }
+
+    public void loseHealth(int damage) {
+        this.hitPoints = this.hitPoints - damage;
+        if (hitPoints <= 0) {
+            System.out.println("Player knock out.");
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHealth() {
+        return hitPoints;
+    }
+
+    public String getWeapon() {
+        return weapon;
+    }
+}
 }
--- 210303Encapsulation/src/com/company/Main.java
+++ 210303Encapsulation/src/com/company/Main.java
@@ -3,13 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Player player = new Player();
-        player.name = "Tim";
-        player.health = 20;
-        player.weapon = "Sword";
+//        Player player = new Player();
+//        player.name = "Tim";
+//        player.health = 20;
+//        player.weapon = "Sword";
+//
+//        int damage = 10;
+//        player.loseHealth(damage);
+//        System.out.println("Remaining health = " + player.healthRemaining());
+//    }
 
-        int damage = 10;
-        player.loseHealth(damage);
-        System.out.println("Remaining health = " + player.healthRemaining());
-    }
+        EnhancedPlayer player = new EnhancedPlayer("Tim",50, "Sword");
+        System.out.println("Initial health is " + player.getHealth());
+
+        
 }
