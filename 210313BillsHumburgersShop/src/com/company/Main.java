0c6503be7811e5c51a0e49da3f5bee364a1193c6
Hikaru Shing <fiveh.5h@gmail.com>
2021-03-13 16:15:41 +0900

    Bills Hamburger Shop

---
+++ 210313BillsHumburgersShop/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+class Humburger {
+    private BreadRollType breadRollType;
+
+
+}
+
+class BreadRollType extends  {
+    private boolean lettuce;
+    private boolean tomatoes;
+    private boolean carrot;
+    private boolean 
+
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
