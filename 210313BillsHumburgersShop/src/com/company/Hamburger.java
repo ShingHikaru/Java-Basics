c9eae434acdd38a093ec92754d7e61da9d6c4996
Hikaru Shing <fiveh.5h@gmail.com>
2021-03-14 15:53:51 +0900

    Updating

--- 210313BillsHumburgersShop/src/com/company/Hamburger.java
+++ 210313BillsHumburgersShop/src/com/company/Hamburger.java
@@ -36,7 +36,7 @@ public class Hamburger {
         this.addition2Name = name;
         this.addition2Price = price;
     }
-}
+
     public void addHamburgerAddition3(String name, double price) {
         this.addition3Name = name;
         this.addition3Price = price;
@@ -52,6 +52,22 @@ public class Hamburger {
         System.out.println(this.name + " hamburger " + " on a " + this.breadRollType + " roll "
                         + "price is " + this.price);
         if (this.addition1Name != null) {
+            hamburgerPrice += this.addition1Price;
             System.out.println("Added " + this.addition1Name + " for an extra " + this.addition1Price);
         }
+        if (this.addition2Name != null) {
+            hamburgerPrice += this.addition2Price;
+            System.out.println("Added " + this.addition2Name + " for an extra " + this.addition2Price);
+        }
+        if (this.addition3Name != null) {
+            hamburgerPrice += this.addition3Price;
+            System.out.println("Added " + this.addition3Name + " for an extra " + this.addition3Price);
+        }
+        if (this.addition4Name != null) {
+            hamburgerPrice += this.addition4Price;
+            System.out.println("Added " + this.addition4Name + " for an extra " + this.addition4Price);
+        }
+        return hamburgerPrice;
+
     }
+}
--- 210313BillsHumburgersShop/src/com/company/Main.java
+++ 210313BillsHumburgersShop/src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Hamburger hamburger = new Hamburger("Basic", "Sausage", 3.56, "White");
+        double price = hamburger.itemizeHamburger();
+
     }
 }
