62e7e023a1d0ebf0ad4fc77e6c30be476080a4af
Hikaru Shing <fiveh.5h@gmail.com>
2021-06-05 07:57:49 +0900

    Updating ArrayChallenge(3)

--- 210529Arrays(Challenge Exercise)/src/com/company/Main.java
+++ 210529Arrays(Challenge Exercise)/src/com/company/Main.java
@@ -9,6 +9,7 @@ public class Main {
     public static void main(String[] args) {
         int[] myIntegers = getIntegers(5);
         int[] sorted = sortIntegers(myIntegers);
+        scanner.next
         printArray(sorted);
 
     }
@@ -45,4 +46,6 @@ public class Main {
             System.out.println("Element " + i + " contents " + array[i]);
         }
     }
+
+
 }
